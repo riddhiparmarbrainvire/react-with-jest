92c171690903afc6baf7620c11da4257723e86cb
Riddhi Parmar <riddhi.parmar@brainvire.com>
2023-11-20 18:15:11 +0530

    first commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+// test (name, function, timeout)
+// name - test name used to identify the test
+// function - function that contains the expectations to test 
+// timeout - optional argument, used to specify how long to wait before aborting the test. the default timeout is 5 secs
+
